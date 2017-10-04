b761d15cac11f9e6111fa3ec7139381a419d74e8
P00L <po775616ol>
2017-10-04 22:17:53 +0200

    upload project

---
+++ TrainApp/app/src/main/java/com/example/pool/trainapp/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.pool.trainapp;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebSocketClient mWebSocketClient;
+    private SeekBar seekBar;
+    private TextView testo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.mySeekBarSx);
+        testo = (TextView) findViewById(R.id.textViewSx);
+        connectWebSocket();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+                testo.setText(String.valueOf(progressChangedValue));
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                testo.setText(String.valueOf("stop" + progressChangedValue));
+                sendMessage("#128");
+
+            }
+        });
+    }
+
+    private void connectWebSocket() {
+        Log.i("Websocket", "Connect");
+        URI uri;
+        try {
+            uri = new URI("ws://192.168.4.1:81");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                Log.i("Websocket", "message " + s);
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+
+    public void sendMessage(String message) {
+        mWebSocketClient.send(message);
+    }
+}
